bfe265bac5ab0bee7527b512c66da67fd08954fc
Tix3Dev <yves.vollmeier.code@gmail.com>
2020-12-31 17:23:55 +0100

    Started working on the execution part

--- src/execution.rs
+++ src/execution.rs
@@ -1,15 +1,17 @@
 pub mod tokenizer;
 
-/*
-let mut predefined_name_order = vec![
-    ("LET", ""),
-    ""
-];
-*/
+use std::collections::HashMap;
+
+enum OrderEnum {
+    SingleOption(Vec<&'static str>),
+    MultipleOptions(Vec<Vec<&'static str>>)
+}
 
 pub fn exec(input: String) {
+    // tokenize the input
     let token_collection = tokenizer::make_tokens(input);
 
+    // check for error
     if let Some((_, value)) = token_collection.iter().find(|(key, _)| key == &"ERROR_MESSAGE") {
         match value {
             tokenizer::ValueEnum::String(v) => {
@@ -21,6 +23,7 @@ pub fn exec(input: String) {
             tokenizer::ValueEnum::TokenVector(_v) => ()
         }
     }
+    // check for comments -> just make a newline
     if let Some((_, value)) = token_collection.iter().find(|(key, _)| key == &"COMMENT") {
         match value {
             tokenizer::ValueEnum::String(_v) => {
@@ -32,7 +35,242 @@ pub fn exec(input: String) {
             tokenizer::ValueEnum::TokenVector(_v) => ()
         }
     }
+    
+    // order of predefined names for checking
+    let predefined_name_order = {
+        let mut hashmap = HashMap::new();
 
+        hashmap.insert("LET", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME", 
+                "VARIABLE/FUNCTION_NAME", 
+                "OPERATOR", 
+                "STRING"
+            ], 
+            vec![
+                "PREDEFINED_NAME", 
+                "VARIABLE/FUNCTION_NAME", 
+                "OPERATOR", 
+                "INTEGER"
+            ]
+        ]));
+
+        hashmap.insert("PRINT", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME", 
+                "STRING"
+            ], 
+            vec![
+                "PREDEFINED_NAME", 
+                "INTEGER"
+            ], 
+            vec![
+                "PREFEINDED_NAME", 
+                "VARIABLE/FUNCTION_NAME"
+            ]
+        ]));
+
+        hashmap.insert("FN", OrderEnum::SingleOption(
+        vec![
+            "PREDEFINED_NAME", 
+            "VARIABLE/FUNCTION_NAME", 
+            "PREDEFINED_NAME"
+        ]));
+
+        hashmap.insert("DO", OrderEnum::SingleOption(
+        vec![
+            "PREDEFINED_NAME", 
+            "VARIABLE/FUNCTION_NAME", 
+        ]));
+
+        hashmap.insert("IF", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "STRING",
+                "OPERATOR",
+                "STRING",
+                "PREDEFINED_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "INTEGER",
+                "OPERATOR",
+                "INTEGER",
+                "PREDEFINED_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "OPERATOR",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME"
+            ]
+        ]));
+
+        hashmap.insert("WHILE", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "STRING",
+                "OPERATOR",
+                "STRING",
+                "PREDEFINED_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "INTEGER",
+                "OPERATOR",
+                "INTEGER",
+                "PREDEFINED_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "OPERATOR",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME"
+            ]
+        ]));
+
+        hashmap.insert("PUSH", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "STRING",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "INTEGER",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME"
+            ]
+        ]));
+
+        hashmap.insert("POP", OrderEnum::SingleOption (
+        vec![
+            "PREDEFINED_NAME",
+            "PREDEFINED_NAME",
+            "VARIABLE/FUNCTION_NAME"
+        ]));
+
+        hashmap.insert("INSERT", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "STRING",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "INTEGER",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ]
+        ]));
+
+        hashmap.insert("REMOVE", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ]
+        ]));
+
+        hashmap.insert("REMOVE", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ]
+        ]));
+        
+        hashmap.insert("GET", OrderEnum::MultipleOptions(
+        vec![
+            vec![
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME",
+                "INTEGER"
+            ],
+            vec![ // LEN
+                "PREDEFINED_NAME",
+                "PREDEFINED_NAME",
+                "VARIABLE/FUNCTION_NAME",
+                "PREDEFINED_NAME"
+            ]
+        ]));
+
+        hashmap
+    };
+
+    match predefined_name_order.get("LET") {
+        Some(value) => {
+            match value {
+                OrderEnum::SingleOption(v) => println!("It's single: {:?}", v),
+                OrderEnum::MultipleOptions(v) => println!("It's not single: {:?}", v)
+            }
+        },
+        None => ()
+    }
     println!("{:?}", token_collection);
 }    
 
