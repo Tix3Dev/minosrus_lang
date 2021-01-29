56c7637ec5379a649fb3eb83383fe91ea7ef70cb
Tix3Dev <yves.vollmeier.code@gmail.com>
2021-01-29 23:10:18 +0100

    Added working INSERT execution

--- src/execution.rs
+++ src/execution.rs
@@ -779,7 +779,62 @@ pub fn exec(input: String, global_variables: &mut HashMap<String, tokenizer::Val
                 }
             }
             else if v == &"INSERT".to_string() {
-                //
+                match &token_collection[3].1 {
+                    tokenizer::ValueEnum::String(stuff) => {
+                        match global_variables.get(stuff) {
+                            Some(value) => {
+                                match &value {
+                                    tokenizer::ValueEnum::IntegerArray(i_vec) => {
+                                        match &token_collection[1].1 {
+                                            tokenizer::ValueEnum::Integer(stuff_to_push) => {
+                                                match &token_collection[5].1 {
+                                                    tokenizer::ValueEnum::Integer(index_where_to_insert) => {
+                                                        let mut i_vec_clone = i_vec.clone();
+                                                        i_vec_clone.insert(*index_where_to_insert as usize, *stuff_to_push);
+                                                        *global_variables.get_mut(stuff).unwrap() = tokenizer::ValueEnum::IntegerArray(i_vec_clone);
+                                                    },
+                                                    _ => unreachable!("SOMEHOW THIS SHOUDLN'T BE PRINTED!")
+                                                }
+                                            },
+                                            _ => {
+                                                println!("EXECUTION ERROR: YOU HAVE TO PUSH AN INTEGER ONTO A INTEGER ARRAY!");
+                                                return;
+                                            }
+                                        }
+                                    },
+                                    tokenizer::ValueEnum::StringArray(s_vec) => {
+                                        match &token_collection[1].1 {
+                                            tokenizer::ValueEnum::String(stuff_to_push) => {
+                                                match &token_collection[5].1 {
+                                                    tokenizer::ValueEnum::Integer(index_where_to_insert) => {
+                                                        let mut s_vec_clone = s_vec.clone();
+                                                        s_vec_clone.insert(*index_where_to_insert as usize, stuff_to_push.to_string());
+                                                        *global_variables.get_mut(stuff).unwrap() = tokenizer::ValueEnum::StringArray(s_vec_clone);
+                                                    },
+                                                    _ => unreachable!("SOMEHOW THIS SHOUDLN'T BE PRINTED!")
+                                                }
+                                            },
+                                            _ => {
+                                                println!("EXECUTION ERROR: YOU HAVE TO PUSH AN INTEGER ONTO A INTEGER ARRAY!");
+                                                return;
+                                            }
+                                        }
+                                    },
+                                    _ => {
+                                        println!("EXECUTION ERROR: YOU CAN ONLY PUSH ONTO ARRAYS!");
+                                        return;
+                                    }
+                                }
+                            },
+                            None => {
+                                println!("EXECUTION ERROR: THERE IS NO VARIABLE CALLED {}", stuff);
+                                return;
+                            }
+                        }
+                    },
+                    _ => unreachable!("SOMEHOW THIS SHOULDN'T BE PRINTED!")
+                }
+
             }
             else if v == &"REMOVE".to_string() {
                 //
