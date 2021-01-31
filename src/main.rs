88548c58ec767969fe9ad4f722d098c0f23daaeb
Tix3Dev <yves.vollmeier.code@gmail.com>
2021-01-31 21:56:55 +0100

    First changes to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,43 @@ mod execution;
 use std::io::{self, Write};
 use std::collections::HashMap;
 
+// here are all variables for exec saved
+pub struct ExecData {
+    // here are all the global variables stored; not changed after one loop iteration
+    pub global_variables: HashMap<String, execution::tokenizer::ValueEnum>,
+    
+    // save state of indentation
+    pub indentation: String,
+
+    // here is all block code saved (except for function code)
+    pub block_code: Vec<Vec<(String, execution::tokenizer::ValueEnum)>>,
+
+    // here are all functions saved 
+    pub functions: HashMap<String, Vec<Vec<(String, execution::tokenizer::ValueEnum)>>>,
+
+    // keep track of the current block code type (normal or functions)
+    pub current_block_type: (String, String)
+}
+
+impl ExecData {
+    pub fn new(a, b, c, d, e) -> Self {
+        Self {
+            global_variables: a,
+            indentation: b,
+            block_code: c,
+            functions: d,
+            current_block_type: e
+        }
+    }
+}
+
 fn main() {
     // some starting text
     println!("EVERYTHING HAS TO BE UPPERCASE!");
     println!("DON'T USE VERINES! THEY AREN'T WORKING PROPERLY");
 
-    // here are all the global variables stored; not changed after one loop iteration
-    let mut global_variables: HashMap<String, execution::tokenizer::ValueEnum> = HashMap::new();
-
-    // save state of indentation
-    let mut indentation = "".to_string();
-
-    // here is all block code saved (except for function code)
-    let mut block_code: Vec<Vec<(String, execution::tokenizer::ValueEnum)>> = Vec::new();
-
-    // here are all functions saved 
-    let mut functions: HashMap<String, Vec<Vec<(String, execution::tokenizer::ValueEnum)>>> = HashMap::new(); 
-
-    // keep track of the current block code type (normal or functions)
-    let mut current_block_type = ("".to_string(), "".to_string());
+    // make ExecData instance
+    let mut exec_data_variable = ExecData::new(HashMap::new(), "".to_string(), Vec::new(), HashMap::new(), ("".to_string(), "".to_string()));
 
     // one loop iteration = one prompt (" >")
     loop {
@@ -33,7 +51,7 @@ fn main() {
         match io::stdin().read_line(&mut input) { // reading input
             Ok(_) => {
                 if !(input.trim().to_string().is_empty()) {
-                    execution::exec(input, &mut global_variables, &mut indentation, &mut block_code, &mut functions, &mut current_block_type);
+                    execution::exec(input, exec_data_variable);
                 }
             },
 
